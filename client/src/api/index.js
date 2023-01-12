19555ec73463a4138ebb48dcc227195ae09e358a
Ankit Kumar <ankitkumardos47@gmail.com>
2023-01-12 22:54:05 +0530

    edited frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API=axios.create({baseURL:'http://localhost:5000/'});
+const API=axios.create({baseURL:'https://megaloop.vercel.app/'});
 
 export const fetchAll=()=>API.get(`/`);
 export const createPost=(newPost)=> API.post('/post',newPost);
